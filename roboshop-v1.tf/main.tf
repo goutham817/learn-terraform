9246071099b5b2be8fd0f468f5f8dc421eb33799
GOUTHAM <goutamreddy817@gmail.com>
2025-02-07 19:49:06 +0530

    terraform

--- roboshop-v1.tf/main.tf
+++ roboshop-v1.tf/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = ami-09c813fb71547fc4f
+  ami           = 973714476881
   instance_type = "t3.small"
   vpc_security_group_ids = sg-04bbe439f126319e7
 
