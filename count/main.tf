fb4ce88cc52fc5bedc8accc900139d9dedc682e7
GOUTHAM <goutamreddy817@gmail.com>
2025-02-11 14:10:38 +0530

    terraform

--- count/main.tf
+++ count/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "instance" {
   count = length(var.instance)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04bbe439f126319e7"]
+  vpc_security_group_ids = ["sg-0f0129348e05b5636"]
   tags = {
     Name = var.instance[count.index]
   }
--- for_each/main.tf
+++ for_each/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "instances" {
   for_each = var.instances
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04bbe439f126319e7"]
+  vpc_security_group_ids = ["sg-0f0129348e05b5636"]
   tags = {
     Name = each.key
   }
